45e44217b0699aa21c8e0de950ffe83457adab18
Almoiz <almoizs@heaptrace.net>
2024-05-11 15:44:08 +0530

    first commit

---
+++ Program.java
@@ -0,0 +1,13 @@
+public class Program{
+    public static void main(String[]args){
+        System.out.println("I am Learning Smart Engineer Begineer Course");
+        System.out.println("I am enjoying Learning Smart Engineer Begineer Course");
+        System.out.println("I have Completed Smart Engineer Begineer Course");
+    
+    
+    
+    
+    }
+    
+    
+    }
